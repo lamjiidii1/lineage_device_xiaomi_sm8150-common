df847b5c8658feb53f137dc7623729b7789dec02
lamjiidii1 <lamjiidii1@gmail.com>
2021-03-12 18:29:56 +0100

    Build new aidl qti power HAL

--- sm8150.mk
+++ sm8150.mk
@@ -275,7 +275,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi_sm8150
+    android.hardware.power-service-qti
 
 # QTI
 PRODUCT_PACKAGES += \
